eb17f45099a31e6dc27ad355cca47a786ac130af
mousamdhakal <mousamdhakal7.md@gmail.com>
2020-07-02 09:27:36 +0545

    Add flappy bird

---
+++ flappy-bird/scripts/obstacles.js
@@ -0,0 +1,58 @@
+function obstacleHandler(parent) {
+  if (parent.frameNo == 1 || parent.everyInterval(200)) {
+    x = parent.canvas.width;
+    height = Math.floor(
+      Math.random() * (MAXHEIGHT - MINHEIGHT + 1) + MINHEIGHT
+    );
+    // create new obstacle if number of obstacles is less than 10
+
+    if (parent.obstacleNumber < 8) {
+      parent.obstaclesList.push(
+        new Component(
+          84,
+          502,
+          this.img,
+          x,
+          PIPE_OFFSET - height,
+          parent,
+          'image',
+          87,
+          503,
+          42,
+          251
+        )
+      );
+      parent.obstacleNumber++;
+      parent.obstaclesList.push(
+        new Component(
+          84,
+          501,
+          this.img,
+          x,
+          GAP - height,
+          parent,
+          'image',
+          43,
+          503,
+          42,
+          251
+        )
+      );
+      parent.obstacleNumber++;
+    }
+    // Reposition old obstacle if number of obstacle is more than 10
+    else {
+      var index = parent.obstacleNumber % 8;
+      parent.obstaclesList[index].x = x;
+      parent.obstaclesList[index].y = PIPE_OFFSET - height;
+      parent.obstacleNumber++;
+      parent.obstaclesList[index + 1].x = x;
+      parent.obstaclesList[index + 1].y = GAP - height;
+      parent.obstacleNumber++;
+    }
+  }
+  for (i = 0; i < parent.obstaclesList.length; i++) {
+    parent.obstaclesList[i].x += -1;
+    parent.obstaclesList[i].update(parent);
+  }
+}
---
+++ flappy-bird/scripts/script.js
@@ -0,0 +1,43 @@
+var keyPressed = false;
+var mainContainer = document.getElementById('main');
+var startContainer = document.getElementById('start-wrapper');
+var score1 = document.getElementById('score-1');
+var score2 = document.getElementById('score-2');
+var bestscore1 = document.getElementById('bestscore-1');
+var bestscore2 = document.getElementById('bestscore-2');
+var endContainer1 = document.getElementById('end-wrapper-1');
+var endContainer2 = document.getElementById('end-wrapper-2');
+var game1;
+
+function startGame() {
+  startContainer.style.display = 'none';
+  game1 = new Game(32, endContainer1, score1, bestscore1);
+  game2 = new Game(87, endContainer2, score2, bestscore2);
+  document.addEventListener('keydown', function (e) {
+    if (!keyPressed) {
+      game1.reactToKeyDown(e.keyCode);
+      game2.reactToKeyDown(e.keyCode);
+      keyPressed = true;
+    }
+  });
+
+  document.addEventListener('keyup', function (e) {
+    game1.reactToKeyUp(e.keyCode);
+    game2.reactToKeyUp(e.keyCode);
+    pressedKey = null;
+    keyPressed = false;
+  });
+}
+
+function restartGame1() {
+  game1.endContainer.style.display = 'none';
+  game1.resetGame();
+  endContainer1.style.marginLeft = CANVAS_WIDTH + 'px';
+  endContainer2.style.marginLeft = '0px';
+}
+function restartGame2() {
+  game2.endContainer.style.display = 'none';
+  game2.resetGame();
+  endContainer2.style.marginLeft = CANVAS_WIDTH + 'px';
+  endContainer1.style.marginLeft = '0px';
+}
